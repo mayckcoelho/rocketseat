6cfcde03b088086874e74f5f182d32193900327e
mayckcoelho <mayckcoelho0@gmail.com>
2019-04-10 22:06:36 -0300

    Comit final do backend.

--- rocketbox/backend/src/controllers/FileController.js
+++ rocketbox/backend/src/controllers/FileController.js
@@ -1,10 +1,21 @@
+const Box = require('../models/Box');
 const File = require('../models/File');
 
 class FileController {
     async store(req, res) {
-        console.log(res.file);
-        // Criar um arquivo
-        return res.send('OK')
+        const box = await Box.findById(req.params.id)
+
+        const file = await File.create({
+            title: req.file.originalname,
+            path: req.file.key,
+        });
+
+        box.files.push(file);
+        await box.save();
+
+        req.io.sockets.in(box._id.emit('file', file));
+
+        return res.json(file);
     }
 }
 
--- rocketbox/backend/src/routes.js
+++ rocketbox/backend/src/routes.js
@@ -7,7 +7,11 @@ const routes = express.Router();
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
-routes.post('/boxes', BoxController.store)
-routes.post('/files', multer(multerConfig).single('file'), FileController.store);
+routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+
+routes.post(
+    '/boxes/:id/files',
+    multer(multerConfig).single('file'), FileController.store);
 
 module.exports = routes;
